606874f1bc73cdc3f60a16185545738dbfd8899d
Benjamin HUBERT <benjamin@alpixel.fr>
2016-02-16 18:37:44 +0100

    ~wip~

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -10,19 +10,29 @@ class SecurityController extends BaseController
 {
     public function renderLogin(array $data)
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!empty($user) && $user != 'anon.') {
-            return new RedirectResponse($this->container->get('router')->generate('front_home'));
-        }
+        $firewalls = $this->container->getMethod('security.firewall.map');
 
-        $requestAttributes = $this->container->get('request')->attributes;
-        if ($requestAttributes->get('_route') == 'admin_login') {
-            $template = 'UserBundle:admin:pages/back_login.html.twig';
-        } elseif ($requestAttributes->get('_route') == 'front_login') {
-            $template = 'AccountBundle:front:pages/front_login.html.twig';
-        } else {
-            $template = 'AccountBundle:front:blocks/block_login.html.twig';
+        dump($firewalls);
+        foreach ($firewalls as $firewall) {
+            dump($firewall);
         }
+        die;
+        // $token = $this->container->get('security.context')->getToken();
+
+        // if
+        // $providerKey = $securitContext->getToken()->getProviderKey();
+        // dump($providerKey);die;
+
+        // $firewallTemplates = $this->container->getParameter('alpixel_user.firewall_templates');
+        // $requestAttributes = $this->container->get('request')->attributes;
+
+        // if ($requestAttributes->get('_route') == 'admin_login') {
+        //     $template = 'UserBundle:admin:pages/back_login.html.twig';
+        // } elseif ($requestAttributes->get('_route') == 'front_login') {
+        //     $template = 'AccountBundle:front:pages/front_login.html.twig';
+        // } else {
+        //     $template = 'AccountBundle:front:blocks/block_login.html.twig';
+        // }
 
         return $this->container->get('templating')->renderResponse($template, $data);
     }
