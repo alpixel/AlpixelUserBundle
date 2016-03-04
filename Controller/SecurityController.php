1618377aa69a474f25d5e5eab42ce6c82aea8362
Benjamin Hubert <benjamin@alpixel.fr>
2016-03-04 11:33:01 +0100

    Debugging bad template name comparison

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -15,12 +15,13 @@ class SecurityController extends BaseController
 
         $firewallTemplates = $this->container->getParameter('alpixel_user.firewall_templates');
         foreach ($firewallTemplates as $templateParam) {
-            if (strstr($templateParam['login_path'], $request->getPathinfo()) !== false) {
+            $path = str_replace('/'.$request->getLocale(), '', $request->getPathInfo());
+            if ($templateParam['login_path'] === $path) {
                 $template = $templateParam['login_template'];
                 break;
             }
         }
-
+        
         return $this->container->get('templating')->renderResponse($template, array_merge($data, [
             'background_image' => $this->container->getParameter('alpixel_user.default_login_background_image'),
             'color'            => $this->container->getParameter('alpixel_user.default_login_background_color'),
